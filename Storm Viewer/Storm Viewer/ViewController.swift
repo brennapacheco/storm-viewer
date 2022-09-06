5adaa588963aa5dc8568c354918e888963acf39f
Brenna <brennapacheco@hotmail.com>
2022-09-06 16:54:54 -0300

    Images listed with file manager

--- Storm Viewer/Storm Viewer/ViewController.swift
+++ Storm Viewer/Storm Viewer/ViewController.swift
@@ -9,9 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var pictures: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let fm = FileManager.default
+        
+        guard let path = Bundle.main.resourcePath else { return }
+        
+        do {
+            
+            let items = try fm.contentsOfDirectory(atPath: path)
+            
+            for item in items {
+                if item.hasPrefix("nssl") {
+                    pictures.append(item)
+                }
+            }
+            
+        } catch {
+            print("Failed to save images.")
+        }
+        
+        print(pictures)
     }
 
 
