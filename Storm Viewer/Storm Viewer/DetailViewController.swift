6946222490ed401c368d8f30ea616107765236ce
Brenna <brennapacheco@hotmail.com>
2022-09-08 08:59:54 -0300

    Add Detail View

---
+++ Storm Viewer/Storm Viewer/DetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.swift
+//  Storm Viewer
+//
+//  Created by Brenna Pacheco da Silva Alves on 08/09/22.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
